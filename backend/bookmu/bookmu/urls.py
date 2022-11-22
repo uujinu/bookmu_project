80e069eb4ae5ce51cb32248a13396abcc7618dce
youjin Cho <ud071233@gmail.com>
2022-11-22 23:40:08 +0900

    Merge branch 'main' of https://github.com/uujinu/bookmu_project into main

--- backend/bookmu/bookmu/urls.py
+++ backend/bookmu/bookmu/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('posts.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
 ]
